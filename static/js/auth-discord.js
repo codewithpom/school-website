6e60bf85d67936d9c1970bb111540455d62f6b5e
Amirhossein Veysi <amirhossein_veysi@yahoo.com>
2021-09-19 14:49:50 +0430

    Animation added

--- static/js/auth-discord.js
+++ static/js/auth-discord.js
@@ -1,4 +1,4 @@
-import { sndRqst } from "./send-form.js";
+import { sndRqst, btnHtml } from "./send-form.js";
 
 $(document).ready(function () {
     //Variable
@@ -12,12 +12,18 @@ $(document).ready(function () {
 
     //Event handler
     function deFrmSubmtHandler(event) {
+        event.preventDefault();
         if ($.trim($('.auth form input').val()) === ''){
-            event.preventDefault();
             $('.auth form .alert-danger').html('Please enter a code to join!').removeClass('d-none');
         }else{
-            let res = sndRqst('auth/', '.auth form');
-            console.log(res);
+            sndRqst('auth/', '.auth form').then(res => {
+                $('button[type = "submit"]').html(btnHtml).removeClass('loading');
+                if (res === 'Done'){
+                    shwSucess();
+                }else{
+                    $('.auth form .alert-danger').html('Something went wrong!').removeClass('d-none');
+                }
+            });
         }
     }
 
@@ -36,4 +42,32 @@ $(document).ready(function () {
     }
 
     //Function
+    function shwSucess() {
+        let html = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Bootstrap Example</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../static/libraries/bootstrap/css/index.css">
+    <link rel="stylesheet" href="../static/css/email-checked.css">
+</head>
+<body>
+
+<div class="container">
+    <div class="row d-flex align-items-center">
+        <div class="col-10 offset-1 col-md-8 offset-md-2 bg-white p-5 rounded mail-sent">
+            <p class="h2 text-center text-secondary">Your Account Verified</p>
+            <img class="mt-4" src="../static/img/open-email-message-svgrepo-com.svg" alt="">
+            <p class="h5 text-center text-secondary mt-5">Welcome to our customers community, You can <a class="text-secondary" href="user.html">Login</a></p>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
+`;
+
+        document.write(html);
+    }
 });
--- static/js/send-form.js
+++ static/js/send-form.js
@@ -1,4 +1,9 @@
-export function sndRqst(url, form) {
+export  let btnHtml = $('button[type = "submit"]').html();
+
+export async function sndRqst(url, form) {
+    //Loading animation
+    $('button[type = "submit"]').html('<img src="../static/img/Rolling-1s-200px.svg" alt="">').addClass('loading');
+
     let data = new FormData(document.querySelector('form')); // Create the data to send
     let xhtp; //To store the request object
     let response; //To return the response from server
@@ -10,15 +15,15 @@ export function sndRqst(url, form) {
         xhtp = new ActiveXObject('Microsoft.XMLHTTP');
     }
 
-    xhtp.open('POST', url);
-    xhtp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    xhtp.send(data);
+    await xhtp.open('POST', url);
+    await xhtp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    await xhtp.send(data);
 
-    xhtp.addEventListener('readystatechange', () => {
+    await xhtp.addEventListener('readystatechange', () => {
         if (xhtp.status === 200 && xhtp.readyState === 4){
             response = xhtp.responseText;
         }
     });
 
-    return response;
+    return await response;
 }
