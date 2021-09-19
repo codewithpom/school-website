9b8ab757e40f577ee7394bdcbc7a7379c8603b0d
Amirhossein Veysi <amirhossein_veysi@yahoo.com>
2021-09-19 14:17:02 +0430

    Announcement added

---
+++ static/js/auth-discord.js
@@ -0,0 +1,39 @@
+import { sndRqst } from "./send-form.js";
+
+$(document).ready(function () {
+    //Variable
+
+    //Event listeners
+    $('.auth form').on('submit', deFrmSubmtHandler);
+
+    $('input').on('blur', deInpBlrHandler).on('focus', deInpFcsHandler);
+
+    $('label').on('click', deLblClckHandler);
+
+    //Event handler
+    function deFrmSubmtHandler(event) {
+        if ($.trim($('.auth form input').val()) === ''){
+            event.preventDefault();
+            $('.auth form .alert-danger').html('Please enter a code to join!').removeClass('d-none');
+        }else{
+            let res = sndRqst('auth/', '.auth form');
+            console.log(res);
+        }
+    }
+
+    function deInpBlrHandler() {
+        if ($.trim($(this).val()) !== ''){
+            $(this).next().css('opacity', 0);
+        }
+    }
+
+    function deInpFcsHandler() {
+        $(this).next().css('opacity', 1);
+    }
+
+    function deLblClckHandler() {
+        $(this).prev().focus();
+    }
+
+    //Function
+});
